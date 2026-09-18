671fc7e1ebb101f676ed018413a1852280ab5d41
Harshit Chauhan <harshit02032001@gmail.com>
2026-09-18 15:25:21 +0530

    add frontend production config

---
+++ frontend/src/config.js
@@ -0,0 +1,23 @@
+// const config = {
+//     env:'local',
+//     // apiUrl:'http://192.168.0.165:5501/api/',
+//     // socketapiUrl:'http://192.168.0.165:5501/api/',
+//     // socketapiUrl:'http://192.168.0.167:5501'
+//     // apiUrl:'https://crmapi.nexthikes.com/api/',
+//     socketapiUrl:'https://crmapistaging.nexthikes.com',
+//     // apiUrl: 'https://crmapi.nexthikes.com/api/',
+//     // socketapiUrl:'https://crmapi.nexthikes.com'
+//     apiUrl:'https://crm-backend-wfc7.onrender.com/api/'
+// };
+
+// export default config;
+
+const config = {
+  env: 'production',
+
+  apiUrl: 'https://crm-backend-wfc7.onrender.com/api/',
+
+  socketapiUrl: 'https://crm-backend-wfc7.onrender.com',
+};
+
+export default config;
